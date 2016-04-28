97e1258836d352760ce6bf6fd773f65064ceadfb
Mazzocraft <jean.maz@hotmail.fr>
2016-04-28 02:31:11 +0200

    com4

--- Python/com.py
+++ Python/com.py
@@ -15,12 +15,12 @@ class Serial(threading.Thread):
                 nbrs=len(rest)
                 num=0
                 for i in range (0,nbrs):
-                    if (rest[i]=="-")or(rest[i]=="1")or(rest[i]=="2")or(rest[i]=="3") :
+                    if (rest[i]=="-")or(rest[i]=="0")or(rest[i]=="1")or(rest[i]=="2")or(rest[i]=="3")or(rest[i]=="4")or(rest[i]=="5")or(rest[i]=="6")or(rest[i]=="7")or(rest[i]=="8")or(rest[i]=="9"):
                         self.nb[num] = rest[i]
                         num = num+1
     def connect(self):
         try:
-            self.ser = serial.Serial("COM3", 9600, timeout=1)
+            self.ser = serial.Serial("COM3", 100, timeout=1)
             self.co = True
         except:
             print("Connection error")
